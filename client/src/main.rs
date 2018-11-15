fc6d98c6b7904e3299ca5f82287a6a5a11f7e8b9
Emmanuel Suarez Acevedo <emmanueljs1@gmail.com>
2018-11-14 22:32:33 -0500

    server is asynchrounously reading/writing to all streams, client is asynchrounously reading from stdin / writing to stdout

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,8 +1,47 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
+use std::thread;
+use std::sync::{Arc, RwLock};
 
 fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:25565")?;
-    stream.write(&['h' as u8, 'e' as u8, 'y' as u8])?;
+    let stream = TcpStream::connect("127.0.0.1:25565")?;
+    let arc = Arc::new(RwLock::new(stream));
+    arc.read().expect("Could not lock").set_nonblocking(true)?;
+
+    let mut handles = vec![];
+
+    let stream_lock = Arc::clone(&arc);
+    let read_handle = thread::spawn(move || {
+        loop {
+            let mut buf: [u8; 1024] = [0; 1024];
+            let mut stream = stream_lock.write().expect("Could not lock");
+            match stream.read(&mut buf) {
+                Ok(0) => continue,
+                Ok(n) => {
+                    // TODO (yash): output buf to GUI instead (printing right now for testing)
+                    print!("Server: {}", std::str::from_utf8(&buf[0..n]).unwrap());
+                }
+                Err(_) => continue,
+            };
+        }
+    });
+    handles.push(read_handle);
+
+    let stream_lock = Arc::clone(&arc);
+    let write_handle = thread::spawn(move || {
+        loop {
+            let mut stdin_buf = [0; 1024];
+
+            std::io::stdin().read(&mut stdin_buf).expect("Could not read from stdin");
+            let mut stream = stream_lock.write().expect("Could not lock");
+            stream.write(&stdin_buf).expect("Could not write to stream");
+        }
+    });
+    handles.push(write_handle);
+
+    for handle in handles {
+        handle.join().expect("Error joining threads");
+    }
+
     Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,44 +1,111 @@
 use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::net::{TcpListener, TcpStream, SocketAddr};
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+use std::clone::Clone;
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:25565")?;
+    let streams = Arc::new(RwLock::new(HashMap::<SocketAddr, TcpStream>::new()));
+    let mut handles = vec![];
 
-    let mut streams: Vec<TcpStream> = vec![];
+    /* TODO (megan):
+     let model = ServerModel::new();
 
-    // accept connections and process them serially
+     should have:
+        registerUser() returns id of new user
+        getNickname(id) gets nickname of user
+        changeNickname(id) sets nickname of user
+     */
+
     loop {
-        for mut stream in streams.iter() {
-           let mut buf: [u8; 1] = [0; 1];
-           let mut buffer: String = String::new();
-
-           loop {
-               match stream.read(&mut buf) {
-                   Ok(0) => break,
-                   Ok(_) => {
-                       match std::str::from_utf8(&buf) {
-                           Ok(string) => buffer.push_str(&string),
-                           Err(e) => {
-                               println!("Error reading stream: {}", e);
-                               break;
-                           }
-                       }
-                   }
-                   Err(e) => {
-                       println!("Error reading stream: {}", e);
-                       break;
-                   }
-               }
-           }
-
-           if !buffer.is_empty() {
-               println!("{}", buffer);
-           }
-        }
-
         match listener.accept() {
-            Ok((stream, _)) => streams.push(stream),
-            Err(e) => println!("Error: {}", e),
+            Ok((stream, addr)) => {
+                println!("New client: {}", addr);
+                let mut write_lock = streams.write().expect("Could not lock");
+                write_lock.insert(addr, stream);
+
+                let mut thread_streams = Arc::clone(&streams);
+
+                let handle = thread::spawn(move || {
+                    let mut just_connected = true;
+
+                    /* TODO (megan):
+                        let user_id = model.registerUser();
+                    */
+
+                    loop {
+                        let streams = thread_streams.read().expect("Could not lock");
+
+                        let mut buffer: String = String::new();
+
+                        let mut buf: [u8; 1024] = [0; 1024];
+
+                        let mut stream = streams.get(&addr).expect("Stream not found");
+                        let _ = stream.set_nonblocking(true);
+
+                        loop {
+                            match stream.read(&mut buf) {
+                                Ok(0) => break,
+                                Ok(n) => buffer.push_str(std::str::from_utf8(&buf[0..n]).unwrap()),
+                                Err(_) => break,
+                            };
+                        }
+
+                        if just_connected {
+                            for mut stream in streams.values() {
+                                // TODO (megan): change 'User' to model.getNickname(user_id)
+                                let _ = stream.write(&"User connected\n".to_string().as_bytes());
+                                let _ = stream.flush();
+                            }
+                        }
+
+                         if !buffer.is_empty() {
+                            /* TODO (megan):
+                                -- change buffer from /<cmd> <arguments> to a Command object by implementing
+                                FromStr trait for Command
+
+                                let cmd: Command = buffer.parse::<Command>();
+                                let username = model.getNickname(user_id);
+
+                                -- also implement updateServerModel(server_model, sender_id) method that updates
+                                server model with respect to command
+                                    e.g. NicknameCommand updateServerModel calls server_model changeNickname
+
+                                cmd.update_server_model(server_model, user_id);
+
+                                -- also implement `as_message` that takes in a nickname and prints out
+                                the command as a chat server message
+                                e.g. NicknameCommand => "User0 has changed nickname to Megan"
+                                     MsgCommand with msg="hello!" => "User0: hello!"
+
+                                let cmd_string = cmd.as_message(username);
+                            */
+
+                             for mut stream in streams.values() {
+                                 // TODO: change &buffer to &cmd_string
+                                let _ = stream.write_all(&buffer.as_bytes());
+                                let _ = stream.flush();
+                             }
+                         }
+
+                         just_connected = false;
+                    }
+                 });
+
+                handles.push(handle);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
+            }
         }
     }
+
+    for handle in handles {
+        let _ = handle.join();
+    }
+
+    Ok(())
 }
