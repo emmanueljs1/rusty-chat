6a485f3f2399f1a21ed181acf8b9aeba3a7c7f09
Emmanuel Suarez Acevedo <emmanueljs1@gmail.com>
2018-12-04 19:36:51 -0500

    add clippy suggestions for server

--- server/src/command.rs
+++ server/src/command.rs
@@ -15,15 +15,15 @@ impl Command {
         let mut temp = curr_user;
         temp.push_str(&" has changed nickname to ".to_string());
         temp.push_str(&args);
-        return temp;
+        temp
       },
       Command::MESSAGE(msg) => {
         let mut temp = curr_user;
         temp.push_str(&": ".to_string());
         temp.push_str(&msg);
-        return temp;
+        temp
       },
-      _ => return "Invalid command".to_string()
+      _ => "Invalid command".to_string()
     }
   }
 }
@@ -31,7 +31,7 @@ impl FromStr for Command {
   type Err = Error;
 
   fn from_str(s: &str) -> Result<Self, Self::Err> {
-    if !s.starts_with("/") {
+    if !s.starts_with('/') {
       return Err(Error::new(ErrorKind::Other, "Command should start with /"));
     }
     
